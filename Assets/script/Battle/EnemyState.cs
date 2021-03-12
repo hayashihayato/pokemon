1e43bb89ca11ceab7f4f4a059075d2a72317d20c
hayashi_hayato <zieghurito@gmail.com>
2021-03-13 08:22:05 +0900

    20210313　ランダムエンカウント、データ管理、消えるバグ修正

---
+++ Assets/script/Battle/EnemyState.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class EnemyState : MonoBehaviour
+{
+    PokeStatusData pokedata;
+    PokeStatus pokestatus;
+
+    [SerializeField] Image PokeImage;
+    [SerializeField] Text NameText;
+    [SerializeField] Text LevelText;
+
+    string PokeName;
+    int Level = 5;
+    int HP;
+    int[] PP = new int[4];
+
+    private void Start()
+    {
+        Find();
+        InitPokeData();
+        Draw();
+    }
+
+    void InitPokeData()
+    {
+        this.PokeName = pokestatus.Name;
+        this.HP = pokestatus.maxHp;
+    }
+
+    void Draw()
+    {
+        PokeImage.sprite = pokestatus.image;
+        NameText.text = this.PokeName;
+        LevelText.text = Level.ToString();
+    }
+
+    void Find()
+    {
+        int PokeID = PlayerPrefs.GetInt("SCORE");
+        pokedata = Resources.Load<PokeStatusData>("PokeStatusData");
+        pokestatus = pokedata.PokeStatusList[PokeID];
+        /*var guids = UnityEditor.AssetDatabase.FindAssets("t:PokeStatusData");
+        if (guids.Length == 0)
+        {
+            throw new System.IO.FileNotFoundException("PokeData does not found");
+        }
+
+        var path = AssetDatabase.GUIDToAssetPath(guids[0]);
+        pokedata = AssetDatabase.LoadAssetAtPath<PokeData>(path);*/
+    }
+}
--- Assets/script/Field/enemyencount.cs
+++ Assets/script/Field/enemyencount.cs
@@ -10,8 +10,9 @@ public class enemyencount : MonoBehaviour
     AudioSource audios;
 
     public AudioClip[] clips;
-    int i = 0;
+    int i = 0,encP;
     int encount_num = 9;
+    int[] pokemonid = new int[] { 483, 484, 487 };
     string[] pokemon = new string[] {
         "ポッポ",
         "ポッポ",
@@ -39,10 +40,16 @@ public class enemyencount : MonoBehaviour
         if(other.tag == "Player")
         {
             i = UnityEngine.Random.Range(0,100);
+            encP = Random.Range(0,2);
             Debug.Log(i);
             if(i <= encount_num) {
                 // 出現ポケモン表示
-                Debug.Log(pokemon[i]);
+                //Debug.Log(pokemon[i]);
+
+                //ポケモン抽選決定
+                SetPokeID(pokemonid[encP]);
+                Debug.Log(pokemonid[encP]);
+
                 // シーン遷移
                 FadeManager.Instance.LoadScene ("Battle", 1.0f);
 
@@ -55,9 +62,15 @@ public class enemyencount : MonoBehaviour
 
             }
             else {
-                Debug.Log("出現なし");
+                //Debug.Log("出現なし");
                 cmsc.IsEncount = false;
             }
         }
+    }
+
+    void SetPokeID(int id)
+    {
+        PlayerPrefs.SetInt("SCORE", id);
+        PlayerPrefs.Save();
     }
 }

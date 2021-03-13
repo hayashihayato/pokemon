7d3068b2b6d5b6bbdefa7586b5d8ac4422af744b
DESKTOP-TNVJ3UR\sweet <sweet4182691@gmail.com>
2021-03-13 13:37:41 +0900

    no message

--- Assets/script/Field/MusicDontDestroy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MusicDontDestroy : MonoBehaviour {
-	private void Awake() {
-		DontDestroyOnLoad(gameObject);
-	}
-}

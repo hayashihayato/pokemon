d38a853790f73ad54434cdf542e93b8de1a53945
DESKTOP-TNVJ3UR\sweet <sweet4182691@gmail.com>
2021-03-16 10:55:19 +0900

    やや変更

--- Assets/script/temoti.cs
+++ Assets/script/temoti.cs
@@ -7,6 +7,7 @@ public class temoti : MonoBehaviour {
     [SerializeField] 
     private bool[] PokeFlag = new bool[6];
     int i = 0;
+    
     public enum Pokemon {
 	    poke1,poke2,poke3,poke4,poke5,poke6
     };

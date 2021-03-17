3e22a68e0c4e3d804503f399fa6916dfbdaede79
DESKTOP-TNVJ3UR\sweet <sweet4182691@gmail.com>
2021-03-17 11:22:22 +0900

    ステータスの実装

--- Assets/script/Battle/EnemyState.cs
+++ Assets/script/Battle/EnemyState.cs
@@ -11,11 +11,26 @@ public class EnemyState : MonoBehaviour
 
     [SerializeField] Image PokeImage;
     [SerializeField] Text NameText;
+    [SerializeField] Text NickNameText;
+    [SerializeField] Text IDText;
     [SerializeField] Text LevelText;
+    [SerializeField] Text HPText;
+    [SerializeField] Text AtkText;
+    [SerializeField] Text DefText;
+    [SerializeField] Text SpAtkText;
+    [SerializeField] Text SpDefText;
+    [SerializeField] Text SpeedText;
 
     string PokeName;
-    int Level = 5;
+    string Nickname;
+    int Level;
     int HP;
+    int Atk;
+    int Def;
+    int SpAtk;
+    int SpDef;
+    int Speed;
+    int Id;
     int[] PP = new int[4];
 
     private void Start()
@@ -27,15 +42,36 @@ public class EnemyState : MonoBehaviour
 
     void InitPokeData()
     {
+        this.Level = 50;
         this.PokeName = pokestatus.Name;
-        this.HP = pokestatus.maxHp;
+        this.Nickname = pokestatus.Name;
+        this.Id = pokestatus.ID;
+
+        this.HP = (pokestatus.maxHp * 2 + 31 + 0 /4) * Level / 100 + Level + 10;
+        // 能力値 = (種族値×2＋個体値＋努力値÷4)×レベル÷100＋レベル＋10
+        this.Atk = ((pokestatus.atk * 2 + 31 + 0 / 4) * Level / 100 + 5) ;
+        this.Def = ((pokestatus.def * 2 + 31 + 0 / 4) * Level / 100 + 5) ;
+        this.SpAtk = ((pokestatus.spatk * 2 + 31 + 0 / 4) * Level / 100 + 5) ;
+        this.SpDef = ((pokestatus.spdef * 2 + 31 + 0 / 4) * Level / 100 + 5) ;
+        this.Speed = ((pokestatus.speed * 2 + 31 + 0 / 4) * Level / 100 + 5) ;
+        // 能力値 = {(種族値×2＋個体値＋努力値÷4)×レベル÷100＋5}
     }
 
     void Draw()
     {
         PokeImage.sprite = pokestatus.image;
+
+        LevelText.text = (Level.ToString());
         NameText.text = this.PokeName;
-        LevelText.text = Level.ToString();
+        NickNameText.text = this.Nickname;
+        IDText.text = Id.ToString();
+
+        HPText.text = HP.ToString();
+        AtkText.text = Atk.ToString();
+        DefText.text = Def.ToString();
+        SpAtkText.text = SpAtk.ToString();
+        SpDefText.text = SpDef.ToString();
+        SpeedText.text = Speed.ToString();
     }
 
     void Find()
@@ -43,6 +79,9 @@ public class EnemyState : MonoBehaviour
         int PokeID = PlayerPrefs.GetInt("ENCID");
         pokedata = Resources.Load<PokeStatusData>("PokeStatusData");
         pokestatus = pokedata.PokeStatusList[PokeID];
+
+
+        
         /*var guids = UnityEditor.AssetDatabase.FindAssets("t:PokeStatusData");
         if (guids.Length == 0)
         {
---
+++ Assets/script/DamegeCalc.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamegeCalc : MonoBehaviour
+{
+    MyPokeStatus Mypoke;
+    int a;
+    EnemyState Enpoke;
+    private void Start() {
+        find();
+    }
+
+    public void find(){
+        // Mypoke = GameObject.Find(MyPokeStatus);
+
+        Enpoke = Resources.Load<EnemyState>("EnemyState");
+    }
+
+}
